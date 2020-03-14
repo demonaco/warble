0b744d5384fb8a7007e5ca5257a63a55044adba1
Matt DeMonaco <mdemonaco@gmail.com>
2020-03-14 00:31:56 -0700

    Set up event handler to deliver 500 error.

--- warbler/warbler-server/index.js
+++ warbler/warbler-server/index.js
@@ -1,20 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
+
 const PORT = 8081;
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 //routes below
 
 app.use(function(req, res, next) {
-    let err = new Error("Not Found")
+    let err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
-app.listen(PORT, function(){
+app.use(errorHandler);
+
+app.listen(PORT, function() {
     console.log('server is starting on port ${PORT}');
 }) 
