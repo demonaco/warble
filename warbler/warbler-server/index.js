8c11c740043e6c05b3277975fe4e0ba47a67565d
Matt DeMonaco <mdemonaco@gmail.com>
2020-03-19 20:52:43 -0700

    Setting up message creation.

--- warbler/warbler-server/index.js
+++ warbler/warbler-server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
 
 const PORT = 8081;
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users:/id/messages", messagesRoutes);
 
 //routes below
 
@@ -25,4 +27,4 @@ app.use(errorHandler);
 
 app.listen(PORT, function() {
     console.log('server is starting on port ${PORT}');
-}) 
+});
