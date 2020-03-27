382d8d59a8288d1ed46bb3d33c77a7013849eb75
Matt DeMonaco <mdemonaco@gmail.com>
2020-03-26 18:50:32 -0700

    adding AuthForm for signin and login

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -2,23 +2,32 @@ import React from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
+import AuthForm from "../components/Authform"
 
-
-const Main = () => {
-    return (
-        <div className="container">
-           <Switch>
-           <Route exact path="/" render={props => <Homepage {...props}/> }>
-               </Route>    
-        </Switch> 
-        </div>
-    );
-};
-
+const Main = () => (
+    <div className="container">
+        <Switch>
+            <Route exact path="/" render={props => <Homepage {...props} />} />
+            <Route exact path="/signin" render={props => {
+                return (
+                    <AuthForm buttonText="Log in" heading="Welcome Back." {...props} />
+                )
+            }}
+            />
+            <Route exact path="/signup" render={props => {
+                return (
+                    <AuthForm buttonText="Sign me up" heading="Join Warbler today" {...props} />
+                )
+            }}
+            />
+      </Switch>
+    </div>
+        );
+        
 function mapStateToProps(state) {
     return {
-        currentUser: state.currentUser
+            currentUser: state.currentUser
     };
-};
+};  
 
 export default withRouter(connect(mapStateToProps)(Main));
