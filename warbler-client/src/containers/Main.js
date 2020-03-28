81890db1de944b4617906d662aa06fae1de0d3f8
Matt DeMonaco <mdemonaco@gmail.com>
2020-03-28 16:54:28 -0700

    Setting up errors, added authorization, passing props to Main.js.

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -3,24 +3,25 @@ import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import AuthForm from "../components/Authform";
-import {authUser} from "../store/actions/auth";
+import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors";
 
 
 const Main = props => {
-    const { authUser } = props;
+    const { authUser, errors, removeError } = props;
     return (
     <div className="container">
         <Switch>
             <Route exact path="/" render={props => <Homepage {...props} />} />
             <Route exact path="/signin" render={props => {
                 return (
-                    <AuthForm onAuth={authUser} buttonText="Log in" heading="Welcome Back." {...props} />
+                    <AuthForm removeError={removeError} errors={errors} onAuth={authUser} buttonText="Log in" heading="Welcome Back." {...props} />
                 )
             }}
             />
             <Route exact path="/signup" render={props => {
                 return (
-                    <AuthForm onAuth={authUser} buttonText="Sign me up" heading="Join Warbler today" {...props} />
+                    <AuthForm removeError={removeError} errors={errors} onAuth={authUser} buttonText="Sign me up" heading="Join Warbler today" {...props} />
                 )
             }}
             />
@@ -31,8 +32,9 @@ const Main = props => {
         
 function mapStateToProps(state) {
     return {
-            currentUser: state.currentUser
+            currentUser: state.currentUser,
+            errors: state.errors
     };
 };  
 
-export default withRouter(connect(mapStateToProps, { authUser })(Main));
+export default withRouter(connect(mapStateToProps, { authUser, removeError })(Main));
--- warbler-client/src/store/actions/auth.js
+++ warbler-client/src/store/actions/auth.js
@@ -1,7 +1,11 @@
-import {apiCall} from "../../services/api"
-import { SET_CURRENT_USER } from "../actionTypes"
+import { apiCall } from "../../services/api";
+import { SET_CURRENT_USER } from "../actionTypes";
+import { addError, removeError } from "./errors";
+
+
 
 export function setCurrentUser(user) {
+    // We send this to our redux reuser
     return {
         type: SET_CURRENT_USER,
         user
@@ -10,14 +14,20 @@ export function setCurrentUser(user) {
 
 export function authUser(type, userData) {
     return dispatch => {
+        //wrap our thunk in a promise so we can wait for the API call
         //wait until api call has finished before dispatching
         return new Promise((resolve, reject) => {
             return apiCall("post", `/api/auth/${type}`, userData).then(
-                ({token, ...user}) => {
-                localStorage.setItem("jwtToken", token)
+                ({ token, ...user }) => {
+                localStorage.setItem("jwtToken", token);
                 dispatch(setCurrentUser(user));
-                resolve();
-            });
+                dispatch(removeError())
+                resolve(); //indicate that API call succeeds
+            })
+            .catch(err => {
+                dispatch(addError(err.message));
+                reject(); //indicate the api failed
+            })
         })
     }
 }
