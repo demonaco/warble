41e34b84e788ea1e54a90084a0046588589ec292
Matt DeMonaco <mdemonaco@gmail.com>
2020-04-01 19:40:01 -0700

    fixing type in warbler-server message.js and adding removeMessage to mapping of MessageItem tag

--- warbler-client/src/components/MessageItem.js
+++ warbler-client/src/components/MessageItem.js
@@ -2,8 +2,9 @@ import React from "react";
 import Moment from "react-moment";
 import { Link } from "react-router-dom";
 import DefaultProfileImg from "../images/default-profile-image.jpg";
+import { removeMessage } from "../store/actions/messages";
 
-const MessageItem = ({date, profileImageUrl, text, username}) => (
+const MessageItem = ({ date, profileImageUrl, text, username, removeMessage }) => (
 <div>
     <li className="list-group-item">
     <img src={profileImageUrl || DefaultProfileImg}
@@ -20,6 +21,10 @@ const MessageItem = ({date, profileImageUrl, text, username}) => (
         </Moment>
     </span>
         <p>{text}</p>
+        <a className="btn btn-danger" 
+        onClick={removeMessage}>
+            Delete
+        </a>
     </div>
     </li>
 </div>
--- warbler-client/src/containers/MessageList.js
+++ warbler-client/src/containers/MessageList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchMessages } from "../store/actions/messages";
+import { fetchMessages, removeMessage } from "../store/actions/messages";
 import MessageItem from "../components/MessageItem";
 
 class MessageList extends Component {
@@ -8,14 +8,16 @@ class MessageList extends Component {
         this.props.fetchMessages();
     }
     render() {
-        const { messages } = this.props
+        const { messages, removeMessage } = this.props
         let messageList = messages.map(m => (
             <MessageItem 
             key={m._id} 
             date={m.createAt} 
             text={m.text} 
             username={m.user.username}
-            profileImageUrl={m.user.profileImageUrl}/>
+            profileImageUrl={m.user.profileImageUrl}
+            removeMessage={removeMessage.bind(this, m.user._id, m._id)}
+            />
         ));
         return (
           <div  className="row col-sm-8">
@@ -35,4 +37,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps, { fetchMessages })(MessageList);
+export default connect(mapStateToProps, { fetchMessages, removeMessage })(MessageList);
